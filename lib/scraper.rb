fa877e869cfde1f076f93a9a9880737b1f8f4124
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-02 16:21:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
         student_profile_url = "#{student.attr('href')}"
         student_name = student.css(".student-name").text
         student_location = student.css(".student-location").text
-        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+        students << {name: student_name, location: student_location, profile_url: student_profile_url}
       end
     end
     students
