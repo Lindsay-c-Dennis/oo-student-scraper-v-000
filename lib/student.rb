871ef6b0c3e4f808dd51901bd9f3cd2cdcf04a8c
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-02 16:18:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      Student.new(student)
+    students_array.each do |student_hash|
+      Student.new(student_hash)
       @@all << self 
     end
   end
